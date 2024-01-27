19618b1a742bc92a58c1c1a4c04876b93052fc50
Aliexandr Andrade <aliexandr.andrade@spaceos.io>
2024-01-28 00:03:33 +0100

    increase page size

--- logbook/serializers/bookmark_serializer.py
+++ logbook/serializers/bookmark_serializer.py
@@ -3,9 +3,7 @@ from ..models import Bookmark, Topic
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
-    topics = serializers.PrimaryKeyRelatedField(many=True, queryset=Topic.objects.order_by('created_at'))
-
     class Meta:
         model = Bookmark
-        fields = ('id', 'title', 'user_id', 'created_at', 'updated_at', 'topics')
+        fields = ('id', 'title', 'user_id', 'created_at', 'updated_at')
         read_only_fields = ('user_id',)
